f123d612daf42bff113736cff9c520431da70a9d
NikolasSerg <nikolas.ser.a@yandex.ru>
2020-09-30 13:35:13 +0300

    outputting data to the Post page

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
+const Product = require('../models/product');
 
 const router = Router();
 
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
+    const product = await Product.getAll();
+    console.log(product, ' - product in post.js');
     res.status(200);
     res.render('posts', {
         title: "posts",
-        isPosts: true
+        isPosts: true,
+        product
     });
 })
 
